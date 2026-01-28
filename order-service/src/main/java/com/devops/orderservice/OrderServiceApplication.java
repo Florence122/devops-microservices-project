9fbada5a1fc3ebb9c7b1c68a7ab1245ce267e578
Ntando Miya <ntandomiya97@gmail.com>
2026-01-28 15:52:02 +0200

    feat: Add REST API endpoints for Order Service

--- order-service/src/main/java/com/devops/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/devops/orderservice/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.devops.order_service;
+package com.devops.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
